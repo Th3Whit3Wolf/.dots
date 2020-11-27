fe40d203dedff94797edf689b61011b78555cf8b
David Karrick <the.white.wolf.is.1337@gmail.com>
2020-11-27 14:35:19 +0000

    Added zsh plugins

---
+++ .zsh_plugins.sh
@@ -0,0 +1,3 @@
+fpath+=( /home/doc/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-better-npm-completion )
+fpath+=( /home/doc/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-pkulev-SLASH-zsh-rustup-completion )
+fpath+=( /home/doc/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-buonomo-SLASH-yarn-completion )
