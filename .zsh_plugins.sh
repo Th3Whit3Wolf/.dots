3cc14cab9dea04ebfa198d13b364f38d32500386
David Karrick <the.white.wolf.is.1337@gmail.com>
2020-11-27 14:37:57 +0000

    zsh_plugins.sh is no longer needed

--- .zsh_plugins.sh
+++
@@ -1,3 +0,0 @@
-fpath+=( /home/doc/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-better-npm-completion )
-fpath+=( /home/doc/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-pkulev-SLASH-zsh-rustup-completion )
-fpath+=( /home/doc/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-buonomo-SLASH-yarn-completion )
